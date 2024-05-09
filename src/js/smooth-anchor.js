d828f6d66b4928b96d98c8b1b46d6b35b2304bc9
Mykyta <nikita2001395@gmail.com>
2024-05-09 23:51:19 +0300

    add anchor smooth

---
+++ src/js/smooth-anchor.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href="#leave-an-application"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
