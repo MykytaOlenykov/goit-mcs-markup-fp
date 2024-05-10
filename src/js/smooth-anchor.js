12fcc6711bed6631c6311e82de67d30fe2f6ee22
Mykyta <nikita2001395@gmail.com>
2024-05-10 09:59:07 +0300

    fix styles and add scroll in footer

--- src/js/smooth-anchor.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href="#leave-an-application"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    });
-  });
-});
