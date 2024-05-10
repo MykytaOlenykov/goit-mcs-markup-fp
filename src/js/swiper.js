7ce85345c21bf67223832032ed19560551c3673a
eugene.lavrinenkov@jsp.email <eugene.lavrinenkov@jsp.email>
2024-05-10 18:06:02 +0300

    fix

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,10 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
 
-const swiper = new Swiper(".mySwiper", {
+import 'swiper/css';
+
+const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  modules: [Pagination],
 });
