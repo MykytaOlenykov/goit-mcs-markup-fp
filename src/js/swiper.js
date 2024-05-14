830f9f6cd55086f2d1a571bbb04c4d7e137feb95
eugene.lavrinenkov@jsp.email <eugene.lavrinenkov@jsp.email>
2024-05-14 17:55:15 +0300

    add comments

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,7 +2,9 @@ import Swiper from 'swiper';
 import { Keyboard } from 'swiper/modules';
 import 'swiper/css/bundle';
 
+//slider initialization
 new Swiper('.swiper', {
+  // Optional parameters
   slidesPerView: 1,
   spaceBetween: 32,
   watchOverflow: true,
